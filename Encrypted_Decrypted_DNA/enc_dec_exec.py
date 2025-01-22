2b4a8024c09657eb51ed19912dea0377a8ca55fa
Anhh <136888885+KimAnh148@users.noreply.github.com>
2025-01-23 00:01:31 +0700

    DNA

---
+++ Encrypted_Decrypted_DNA/enc_dec_exec.py
@@ -0,0 +1,16 @@
+from dna_get import main as encrypt
+from dna_gdt import main as decrypt
+
+import os
+
+if __name__ == '__main__':
+    # run encryption
+    print("\n######## ENCRYPTION ########")
+    encrypt()
+
+    # run decryption
+    print("\n######## DECRYPTION ########")
+    decrypt()
+
+
+
